5debabad7338d6b9bebcfeb74c9e4bbe0ee8a030
SabrinaSchloegl <sabrinasch8910@gmail.com>
2022-07-08 21:08:02 +0200

    daily forecast lat lon added - console log
    API Key, console log coord and data.daily

--- src/index.js
+++ src/index.js
@@ -29,7 +29,8 @@ function formatDay(timestamp) {
 
   return days[day];
 }
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
@@ -44,7 +45,7 @@ function displayForecast() {
         <img
           src="http://openweathermap.org/img/wn/50d@2x.png"
           alt=""
-          width="42"
+          width="40"
         />
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max"> 18° </span>
@@ -58,6 +59,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
 }
+
+function getForecast(coordinates) {
+	  console.log(coordinates);
+	  let apiKey = "9fa869b82f729f6b388e9602e181cb7f";
+	  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+	  axios.get(apiUrl).then(displayForecast);
+	}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -82,6 +90,7 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
+  getForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = "9fa869b82f729f6b388e9602e181cb7f";
